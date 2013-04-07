5ec3129baa7a5909d604bd7e821898b17a259bb0
Mike Reeves <mreeves@mercantila-inc.com>
2013-04-07 11:04:31 -0700

    _construct instead of __construct... REALLY varien?

--- app/code/community/Zoostores/FrontendPerformanceLogger/Model/Mysql4/Log.php
+++ app/code/community/Zoostores/FrontendPerformanceLogger/Model/Mysql4/Log.php
@@ -1,7 +1,7 @@
 <?php
 class Zoostores_FrontendPerformanceLogger_Model_Mysql4_Log extends Mage_Core_Model_Mysql4_Abstract
 {
-    public function __construct()
+    public function _construct()
     {
         $this->_init('frontendperformancelogger/log', 'id');
     }
