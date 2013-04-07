7a74a672abde6db5d77cbaa017c1178750ea09f8
Mike Reeves <mreeves@mercantila-inc.com>
2013-04-06 19:32:41 -0700

    adding route and url to logger js object

--- app/code/community/Zoostores/FrontendPerformanceLogger/Block/Tag.php
+++ app/code/community/Zoostores/FrontendPerformanceLogger/Block/Tag.php
@@ -8,6 +8,12 @@
 
 class Zoostores_FrontendPerformanceLogger_Block_Tag extends Mage_Core_Block_Template
 {
+    private $_route;
+
+    public function __construct() {
+        parent::__construct();
+    }
+
     public function isEnabled()
     {
         return (bool)Mage::getStoreConfig('zoostores/frontendperformancelogger/active');
@@ -18,5 +24,14 @@ class Zoostores_FrontendPerformanceLogger_Block_Tag extends Mage_Core_Block_Temp
         return (bool)Mage::getStoreConfig('zoostores/frontendperformancelogger/debug');
     }
 
-
+    public function getRoute() 
+    {
+        if (!isset($this->_route)){
+            $module       = Mage::app()->getRequest()->getModuleName();
+            $controller   = Mage::app()->getRequest()->getControllerName();
+            $action       = Mage::app()->getRequest()->getActionName();
+            $this->_route = $module . '_' . $controller . '_' . $action;
+        }
+        return $this->_route;
+    }
 }
